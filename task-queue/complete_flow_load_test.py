0af542f63b8a325973beb005c8d408e4f29285af
Benjamin Smidt <92611223+bensmidt@users.noreply.github.com>
2023-06-03 19:37:18 -0500

    normal queue with locust analytics works

--- task-queue/complete_flow_load_test.py
+++ task-queue/complete_flow_load_test.py
@@ -3,27 +3,29 @@ import requests
 import time
 from locust import HttpUser, task, constant, events
 
-def async_success(name, start_time, resp):
-    events.request_success.fire(
+def async_success(name:str, start_time:float, resp):
+    events.request.fire(
         request_type=resp.request.method,
         name=name,
         response_time=int((time.monotonic() - start_time) * 1000),
-        response_length=len(resp.content),
+        response_length=len(resp.content), 
+        # no exception = successs
     )
 
-def async_failure(name, start_time, resp, message):
-    events.request_failure.fire(
+def async_failure(name:str, start_time:float, resp, message:str):
+    events.request.fire(
         request_type=resp.request.method,
         name=name,
         response_time=int((time.monotonic() - start_time) * 1000),
-        exception=Exception(message),
+        response_length=len(resp.content), 
+        exception=Exception(message), # throw exception = failure
     )
 
 class reportService(HttpUser):
     
     wait_time = constant(1)
 
-    def _do_async_thing_handler(self, timeout=600):
+    def _do_async_thing_handler(self, timeout:float=600):
         post_resp = requests.post(self.host + 'report')
         if not post_resp.status_code == 200:
             return
@@ -34,14 +36,14 @@ class reportService(HttpUser):
         start_time = time.monotonic()
         end_time = start_time + timeout
         while time.monotonic() < end_time:
-            r = self.client.get(self.host + 'report/' + id)
+            r = requests.get(self.host + 'report/' + id)
             if r.status_code == 200 and r.json()['result'] != None:
                 async_success('POST /report/ID - async', start_time, post_resp)
                 return
 
             # IMPORTANT: Sleep must be monkey-patched by gevent (typical), or else
             # use gevent.sleep to avoid blocking the world.
-            time.sleep(1)
+            gevent.sleep(1)
         async_failure('POST /report/ID - async', start_time, post_resp, 'Failed - timed out after %s seconds' % timeout)
 
     @task
